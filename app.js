72d4cda0e66cce5753c1912e6a750ad0c20ba9d2
Liubov M <l.v.mykhailychenko@gmail.com>
2023-06-13 22:06:57 +0200

    add beginning js

---
+++ app.js
@@ -0,0 +1,10 @@
+// Select The Elements
+
+var toggle_btn = document.querySelector(".toggle-btn");
+var big_wrapper = document.querySelector(".big-wrapper");
+const main = document.querySelector("main");
+
+function toggleAnimation () {
+    alert("Heloo");
+}
+toggle_btn.addEventListener('click', toggleAnimation);
